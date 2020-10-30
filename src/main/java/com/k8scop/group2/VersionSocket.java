31c8058ad2f8bfdbf7e3e5a0aa44e593cc26a80b
Wind Hong <hzywind@gmail.com>
2020-10-30 22:26:22 +0800

    Update VersionSocket.java

--- src/main/java/com/k8scop/group2/VersionSocket.java
+++ src/main/java/com/k8scop/group2/VersionSocket.java
@@ -1,12 +1,4 @@
-/*------------------------------------------------------------------------------
- * COPYRIGHT Ericsson 2020
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- *----------------------------------------------------------------------------*/
+
 package com.k8scop.group2;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
